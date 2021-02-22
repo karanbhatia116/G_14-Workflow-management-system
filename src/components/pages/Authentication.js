affe71cf289cc461ed524f95f2a11b6e2a809e38
AlayKharadi <alay.kharadi3@gmail.com>
2021-02-22 22:50:00 +0530

    initial structure

---
+++ src/components/pages/Authentication.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Modal from "react-bootstrap/Modal";
+import NavLink from "react-bootstrap/NavLink";
+import LogIn from "./LogIn";
+import SignUp from "./SignUp";
+import ForgotPassword from "./ForgotPassword";
+
+function Authentication() {
+    const [show, setShow] = useState(true);
+
+    const handleClose = () => setShow(false);
+
+    const [signUp, setSignUp] = useState(<LogIn></LogIn>);
+
+    const [ModalTitle, setModalTitle] = useState("Log-In");
+
+    const SignalSignUp = () => {
+        setSignUp(<SignUp></SignUp>);
+        setModalTitle("SignUp");
+    }
+
+    const SignalForgotPassword = () => {
+        setSignUp(<ForgotPassword></ForgotPassword>);
+        setModalTitle("Forgot Password");
+    }
+
+    return (
+        <Modal show={show} onHide={handleClose} backdrop="static" size="lg" centered>
+            <Modal.Header closeButton>
+                <Modal.Title>
+                    {ModalTitle}
+                </Modal.Title>
+            </Modal.Header>
+            
+            <Modal.Body>
+                {signUp}
+            </Modal.Body>
+
+            <Modal.Footer>
+                <NavLink onClick={SignalForgotPassword}>
+                    Forgot Password?
+                </NavLink>
+                <NavLink onClick={SignalSignUp}>
+                    Don't have account?
+                </NavLink>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+export default Authentication;
---
+++ src/components/pages/Projects.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Jumbotron from "react-bootstrap/Jumbotron";
+class Projects extends React.Component {
+    render() {
+        return (
+            <Jumbotron>
+                <h1>
+                    Project-Page
+                </h1>
+            </Jumbotron>
+        );
+    }
+}
+export default Projects;
